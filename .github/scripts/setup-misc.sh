06b7f4a3b12c0692b0ebf7d472c4f5f843694014
Ian de Jesus <ian.deejay.iv@gmail.com>
2023-08-21 10:52:07 +0800

    feat: add syncthing misc package

--- .github/scripts/setup-misc.sh
+++ .github/scripts/setup-misc.sh
@@ -9,3 +9,10 @@ for PKG in "${PKGS[@]}"; do
     echo "INSTALLING: ${PKG}"
     sudo apt install -y "$PKG"
 done
+
+echo "INSTALLING: syncthing"
+# script taken from: https://apt.syncthing.net/
+sudo curl -o /usr/share/keyrings/syncthing-archive-keyring.gpg https://syncthing.net/release-key.gpg
+echo "deb [signed-by=/usr/share/keyrings/syncthing-archive-keyring.gpg] https://apt.syncthing.net/ syncthing stable" | sudo tee /etc/apt/sources.list.d/syncthing.list
+sudo apt update
+sudo apt install -y syncthing
