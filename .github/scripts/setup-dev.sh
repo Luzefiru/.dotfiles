156f8c39d88ea999df012f75693bdf7420a2f094
Ian de Jesus <ian.deejay.iv@gmail.com>
2023-09-18 05:54:45 +0800

    fix: add current user to docker usergroup to run commands without sudo

--- .github/scripts/setup-dev.sh
+++ .github/scripts/setup-dev.sh
@@ -19,6 +19,7 @@ echo \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt update
 sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo usermod -aG docker $USER
 
 PKGS=(
         'nodejs'                # javascript runtime engine
