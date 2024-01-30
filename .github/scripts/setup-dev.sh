cdb6d846ccb8508944a9664110ba08aa4e8ea4f7
Ian de Jesus <ian.deejay.iv@gmail.com>
2024-01-31 07:52:47 +0800

    feat: add .NET LTS install script

--- .github/scripts/setup-dev.sh
+++ .github/scripts/setup-dev.sh
@@ -51,3 +51,11 @@ sudo chmod 644 /etc/environment
 
 echo "INSTALLING: netbeans (snap)"
 sudo snap install netbeans --classic
+
+echo "INSTALLING: .NET SDK LTS"
+wget https://dot.net/v1/dotnet-install.sh -O dotnet-install.sh
+chmod +x ./dotnet-install.sh
+./dotnet-install.sh
+echo 'export DOTNET_ROOT=$HOME/.dotnet' >> $HOME/.zshrc
+echo 'export PATH=$PATH:$DOTNET_ROOT:$DOTNET_ROOT/tools' >> $HOME/.zshrc
+rm ./dotnet-install.sh
